d72a8b9820af3e2dc6e1eb20566b76fa3d8a63bd
Hardik Hariyani <hardikhariyani86@gmail.com>
2025-11-10 15:11:58 -0600

    Add main Spring Boot application class

---
+++ src/main/java/com/kristechna/KristechnaApplication.java
@@ -0,0 +1,13 @@
+package com.kristechna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KristechnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KristechnaApplication.class, args);
+	}
+
+}
