3dff5e2a95d8d920610757c182779db3c4e55eb3
Hardik Hariyani <hardikhariyani86@gmail.com>
2025-11-11 10:02:00 -0600

    Add Spring Boot configuration and test files

---
+++ src/test/java/com/kristechna/KristechnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristechna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KristechnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
